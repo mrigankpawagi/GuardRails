2e91f12191ebd69976f0cec914b3b78e9551b935
Mrigank Pawagi <mrigankpawagi@gmail.com>
2025-01-11 17:09:59 +0530

    place webview dependencies and fix baseUri issue. minor fixes.

--- media/js/main.js
+++ media/js/main.js
@@ -1,21 +1,21 @@
-document.getElementById('codeForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('codeForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
     
-    var imports = importsEditor.getValue();
-    var functionName = document.getElementById('functionName').value || "function_name";
-    var arguments = document.getElementById('arguments').value;
-    var returnType = document.getElementById('returnType').value;
-    var docstring = docstringEditor.getValue();
+//     var imports = importsEditor.getValue();
+//     var functionName = document.getElementById('functionName').value || "function_name";
+//     var arguments = document.getElementById('arguments').value;
+//     var returnType = document.getElementById('returnType').value;
+//     var docstring = docstringEditor.getValue();
 
-    var fullDeclaration = `${imports}\n\ndef ${functionName}(${arguments})${returnType}:\n    """\n${docstring}\n    """`;
+//     var fullDeclaration = `${imports}\n\ndef ${functionName}(${arguments})${returnType}:\n    """\n${docstring}\n    """`;
 
-    fetch(`/submit?declaration=${encodeURIComponent(fullDeclaration)}`)
-        .then(response => response.text())
-        .then(data => {
-            console.log('Form submitted successfully!');
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-});
+//     fetch(`/submit?declaration=${encodeURIComponent(fullDeclaration)}`)
+//         .then(response => response.text())
+//         .then(data => {
+//             console.log('Form submitted successfully!');
+//             console.log(data);
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// });
